3448e874796e29a1207fc1e077d6dadbbe8fdce7
Dennis Goldfarb <d.goldfarb@wustl.edu>
2024-06-09 13:42:15 -0500

    get pool

--- RIS/ProteomeTools/pipeline.sh
+++ RIS/ProteomeTools/pipeline.sh
@@ -7,7 +7,7 @@ function get_jobid {
 }
 
 function get_pool {
-    POOL_FULL=`awk 'NR==2 {split($1, arr, "-"); print arr[2]}'`
+    POOL_FULL=grep -o -P '\-.*?\-' <<< $1 | head -1
     echo $POOL_FULL
     #POOL=${POOL_FULL::-6}
     #echo $POOL
