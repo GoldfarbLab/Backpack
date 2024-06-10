60d256fcaa5fd74383274600d50b0e4d9baa19b4
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2024-06-10 08:15:03 -0500

    extract EV

--- extractEV.py
+++ extractEV.py
@@ -19,36 +19,10 @@ from ThermoFisher.CommonCore.Data.FilterEnums import IonizationModeType, MSOrder
 from ThermoFisher.CommonCore.Data.Interfaces import IScanEventBase, IScanFilter
 
 parser = argparse.ArgumentParser(
-                    prog='Sage filter',
-                    description='Filter Sage results by ID quality metrics')
-parser.add_argument("sage_results")
+                    prog='eV extractor',
+                    description='get data for NCE to eV mapping')
 parser.add_argument("raw_path")
-parser.add_argument("-m", "--psm_q", default=0.01, type=float)
-parser.add_argument("-p", "--pep_q", default=0.01, type=float)
-parser.add_argument("-e", "--post_error", default=-1, type=float)
 args = parser.parse_args()
-
-
-def NCE2eV(nce, mz, charge):
-    cf = 1.0
-    if charge==2: cf=0.9
-    elif charge==3: cf=0.85
-    elif charge==4: cf=0.8
-    elif charge>=5: cf=0.75
-    ev = nce*(mz/500)*cf
-    return ev
-
-
-#################################################################################
-
-data = pd.read_csv(args.sage_results, sep="\t")
-data = data[data["spectrum_q"] <= args.psm_q]
-data = data[data["peptide_q"] <= args.pep_q]
-data = data[data["posterior_error"] <= args.post_error]
-data = data[data["proteins"].str.contains("pt\|")]
-data = data[data["label"] == 1]
-
-data.to_csv(args.sage_results + ".filtered", sep="\t", index=False, quoting=csv.QUOTE_NONE)
 
 
 #################################################################################
@@ -62,9 +36,10 @@ creation_date = rawFile.FileHeader.CreationDate
 
 print(instrument_model, instrument_id, creation_date)
 
-for index, row in data.iterrows():
-    scan_id = int(row["scannr"].split("=")[-1])
-    
+firstScanNumber = rawFile.RunHeaderEx.FirstSpectrum
+lastScanNumber = rawFile.RunHeaderEx.LastSpectrum
+
+for scan_id in range(firstScanNumber, lastScanNumber):
     # Get the scan filter for this scan number
     scanFilter = IScanFilter(rawFile.GetFilterForScanNumber(scan_id))
 
@@ -76,7 +51,6 @@ for index, row in data.iterrows():
     if scanFilter.MSOrder == MSOrderType.Ms2:
         reaction = scanEvent.GetReaction(0)
         collisionEnergy = reaction.CollisionEnergy
-        isolationWidth = reaction.IsolationWidth
         
         # Get the trailer extra data for this scan and then look
         # for the monoisotopic m/z value in the trailer extra data
@@ -90,12 +64,10 @@ for index, row in data.iterrows():
             if k == "Charge State:":
                 key2val["z"] = v
                 if int(v) < 1 or int(v) > 8: continue
-            elif k == "Orbitrap Resolution:":
-                key2val["resolution"] = v
             elif k == "HCD Energy:":
                 key2val["NCE"] = v
-            elif k == "RawOvFtT:":
-                key2val["RawOvFtT"] = v
+            elif k == "HCD Energy V:":
+                key2val["eV"] = v
         
         filterString = scanFilter.ToString()
         
@@ -104,13 +76,7 @@ for index, row in data.iterrows():
         key2val["Reaction Type"] = re.findall("[a-zA-Z]+", filterString.split()[-2].split("@")[1])[0]
         if key2val["Reaction Type"] != "hcd": continue
         
-        key2val["Analyzer"] = filterString.split()[0]
         key2val["Isolation Center"] = filterString.split()[-2].split("@")[0]
-        key2val["NCE"] = re.split("[a-zA-Z]+", filterString.split()[-2].split("@")[1])[1]
-        key2val["LowMz"] = filterString.split()[-1].split("-")[0][1:]
-        key2val["HighMz"] = filterString.split()[-1].split("-")[1][0:-1]
-        key2val["Scan Filter"] = filterString
-        key2val["eV"] = NCE2eV(float(key2val["NCE"]), float(key2val["Isolation Center"]), int(key2val["z"]))
         
         print(key2val)
         sys.exit()
